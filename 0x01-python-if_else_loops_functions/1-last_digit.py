1487be5bb92c7e5e9f88d73556995a04c9e6ead6
jaarmore <jaarmore@gmail.com>
2019-12-10 21:55:42 +0000

    Fix code that exceds the limits of chars on 1-last_digit.py file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 is_negative = False
 
-if number<0:
+if number < 0:
     is_negative = True
     number *= (-1)
 
-last_digit = number%10  # Get the last digit of the number given
+last_digit = number % 10  # Get the last digit of the number given
 if is_negative:
     last_digit *= -1
     number *= (-1)
@@ -15,6 +15,8 @@ if is_negative:
 if last_digit == 0:
     print('Last digit of {} is {} and is 0'.format(number, last_digit))
 elif last_digit < 6 and last_digit != 0:
-    print('Last digit of {} is {} and is less than 6 and not 0'.format(number, last_digit))
+    print('Last digit of {} is {} and is less than 6 '
+          'and not 0'.format(number, last_digit))
 elif last_digit > 5:
-    print('Last digit of {} is {} and is greater than 5'.format(number, last_digit))
+    print('Last digit of {} is {} and is greater '
+          'than 5'.format(number, last_digit))
