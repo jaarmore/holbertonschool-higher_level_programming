9af9a4d83be939bf3a746b13add9b147ee2bff91
jaarmore <jaarmore@gmail.com>
2019-12-10 23:22:40 +0000

    Adding 5-print_comb2.py python script file to repository

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for number in range(0, 100):
+    if number == 99:
+        print('{}{}'.format((number//10), (number % 10)))
+        break
+    print('{}{}'.format((number//10), (number % 10)), end=', ')
