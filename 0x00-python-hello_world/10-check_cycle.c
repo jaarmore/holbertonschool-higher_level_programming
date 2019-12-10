a3df14c910f3007ec1efa7dd6dbaf33bbc4a33be
jaarmore <jaarmore@gmail.com>
2019-12-10 04:54:16 +0000

    Adding files from task10 to repository

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - function that checks if a singly linked list has a cycle.
+ * @list: firs element of the list
+ * Return: 0 if there is no cycle, 1 otherwise
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list.next;
+	listint_t *slow = list;
+
+	if (list == NULL)
+		return (0);
+
+	while (fast != NULL && fast.next != NULL && slow != NULL)
+	{
+		if (fast == slow)
+			return (1);
+		fast = fast.next.next;
+		slow = slow.next;
+	}
+	return (0);
+}
