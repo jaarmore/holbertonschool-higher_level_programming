ecf972c87403df231fe490e3e406684a23a4d67d
jaarmore <jaarmore@gmail.com>
2019-12-11 03:54:22 +0000

    Fix shebang on 12-fizzbuzz.py file

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
-#!/usr/bin/phyton3
+#!/usr/bin/python3
 def fizzbuzz():
     for fizz_num in range(1, 101):
         if (fizz_num % 5) == 0 and (fizz_num % 3) == 0:
-            print('FizzBuzz', end=' ')
+            print('{}'.format('FizzBuzz'), end=' ')
         elif (fizz_num % 3) == 0:
-            print('Fizz', end=' ')
+            print('{}'.format('Fizz'), end=' ')
         elif (fizz_num % 5) == 0:
-            print('Buzz', end=' ')
+            print('{}'.format('Buzz'), end=' ')
         else:
             print('{:d}'.format(fizz_num), end=' ')
