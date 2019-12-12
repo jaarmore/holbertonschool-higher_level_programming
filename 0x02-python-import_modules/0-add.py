cdefa29051e8e299357b77743b5433a317db2316
jaarmore <jaarmore@gmail.com>
2019-12-12 23:53:07 +0000

    adding 0-add.py python file to repository

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from add_0 import add
+
+def my_add_0():
+    """My add_0 function
+
+    Args:
+        a: first integer equals to 1
+        b: second integer equal to 2
+
+    Result:
+        Prints the add of a + b.
+    """
+    a = 1
+    b = 2
+
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+
+if __name__ == '__main__':
+    my_add_0()
