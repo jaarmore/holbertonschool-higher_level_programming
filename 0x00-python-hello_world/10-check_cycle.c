e8f6e0a58aac5dd9996d3afe9ff5e9eab59c1e04
Jackson Moreno <43226501+jaarmore@users.noreply.github.com>
2019-12-11 20:09:37 -0500

    fix code of pointer on file, after PLD

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,18 +6,18 @@
 */
 int check_cycle(listint_t *list)
 {
-	listint_t *fast = list.next;
+	listint_t *fast = list->next;
 	listint_t *slow = list;
 
 	if (list == NULL)
 		return (0);
 
-	while (fast != NULL && fast.next != NULL && slow != NULL)
+	while (fast != NULL && fast->next != NULL && slow != NULL)
 	{
 		if (fast == slow)
 			return (1);
-		fast = fast.next.next;
-		slow = slow.next;
+		fast = fast->next->next;
+		slow = slow->next;
 	}
 	return (0);
 }
