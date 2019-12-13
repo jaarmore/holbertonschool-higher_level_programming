5c41676073c500f5314753b9333ac91fd234a7f3
jaarmore <jaarmore@gmail.com>
2019-12-13 00:04:40 +0000

    Fix conditional if on 0-add.py file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-
-
-def my_add_0():
-    """My add_0 function
-
-    Args:
-        a: first integer equals to 1
-        b: second integer equal to 2
-
-    Result:
-        Prints the add of a + b.
-    """
-    a = 1
-    b = 2
-
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
 
 if __name__ == '__main__':
-    my_add_0()
+    from add_0 import add
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
