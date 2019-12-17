ef8a3e86a6562ce32fb72444213faed8831afd81
jaarmore <jaarmore@gmail.com>
2019-12-17 19:05:40 +0000

    Fix code when list is empty on 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
+    if len(my_list) > 0:
+        my_list.reverse()
 
-    for elem in my_list:
-        print("{:d}".format(elem))
+        for elem in my_list:
+            print("{:d}".format(elem))
