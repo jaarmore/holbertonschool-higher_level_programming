f43f9d9afc54112b694e1bd381f96c7ce25e578a
jaarmore <jaarmore@gmail.com>
2019-12-19 17:29:21 +0000

    Fix code on new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if my_list:
-        new_list = []
+        new_list = list()
         for elem in my_list:
             if elem == search:
                 new_list.append(replace)
