b873e0a6e00c810e9c7b5ea466109f4b7aaf2c0c
jaarmore <jaarmore@gmail.com>
2019-12-20 01:18:54 +0000

    Fix code on return

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if my_list:
-        new_list = list()
-        for elem in my_list:
-            if elem == search:
-                new_list.append(replace)
-            else:
-                new_list.append(elem)
-        return new_list
+        return ([replace if elem == search else elem for elem in my_list])
