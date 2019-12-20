220b941f24b7072b0448257fd46514f228434816
jaarmore <jaarmore@gmail.com>
2019-12-20 01:27:52 +0000

    Fix code on return

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        sum_list = 0
-        new_list = list(set(my_list))
-        for elem in new_list:
-            sum_list += elem
-        return sum_list
+    sum_list = 0
+    new_list = list(set(my_list))
+    for elem in new_list:
+        sum_list += elem
+    return sum_list
