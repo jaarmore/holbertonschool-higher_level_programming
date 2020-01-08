7887d7af66c7e8eeda336f19cd881b22554b8998
jaarmore <jaarmore@gmail.com>
2020-01-08 12:02:23 -0500

    Adding 0-safe_print_list.py python file to repository

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    if my_list:
+        cont = 0
+        try:
+            for val in range(0, x):
+                print('{}'.format(my_list[val]), end='')
+                cont += 1
+            print()
+        except IndexError:
+            print('')
+        return cont
