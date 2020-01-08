785ac66d2b016b61926793098496ca34ad93b638
jaarmore <jaarmore@gmail.com>
2020-01-08 13:33:28 -0500

    Fix return code on file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,7 @@ def safe_print_list(my_list=[], x=0):
                 print('{}'.format(my_list[val]), end='')
                 cont += 1
             print()
+            return cont
         except:
             print('')
-        return cont
+            return cont
