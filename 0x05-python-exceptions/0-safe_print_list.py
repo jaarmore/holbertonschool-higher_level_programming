13841f00c81e5455210591e195b335120de9ea8f
jaarmore <jaarmore@gmail.com>
2020-01-08 13:16:39 -0500

    Fix except on file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
                 print('{}'.format(my_list[val]), end='')
                 cont += 1
             print()
-        except IndexError:
+        except:
             print('')
         return cont
