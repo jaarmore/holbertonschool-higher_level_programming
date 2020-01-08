0a17f1860b05d959da45340016b3115e596e1b3e
jaarmore <jaarmore@gmail.com>
2020-01-08 13:37:18 -0500

    Fix code on except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,7 @@ def safe_print_list(my_list=[], x=0):
             for val in range(0, x):
                 print('{}'.format(my_list[val]), end='')
                 cont += 1
+        except IndexError:
+            pass
             print()
-            return cont
-        except:
-            print('')
-            return cont
+        return cont
