0c097f9fc4af9bc2649f04683040410bd054d4aa
jaarmore <jaarmore@gmail.com>
2020-01-09 02:29:23 +0000

    Fix indentation on file

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     try:
         if isinstance(value, int):
             print('{:d}'.format(value))
-        return True
+            return True
     except:
         return False
