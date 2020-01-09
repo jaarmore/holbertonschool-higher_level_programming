dff19b616dedfd4a78d1fccf6cf22fa01523b7f6
jaarmore <jaarmore@gmail.com>
2020-01-09 18:22:01 -0500

    Adding 2-square.py python file to repository

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+class Square:
+    """class Square that defines a Square"""
+
+    def __init_(self, size=0):
+        """Defines de first element of the object.
+
+        Args:
+            size: size of the square
+        """
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
