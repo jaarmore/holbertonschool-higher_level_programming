fd5181885dd1d0845916c5f3d05b1d7d38551c11
jaarmore <jaarmore@gmail.com>
2020-01-09 19:35:36 -0500

    Adding 3-square.py file to repository

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square():
+    """class Square that defines a Square"""
+    def __init__(self, size=0):
+        """check if the size of square is an integer."""
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
+
+    def area(self):
+        """Return the area of square"""
+        return (self.__size * self.__size)
