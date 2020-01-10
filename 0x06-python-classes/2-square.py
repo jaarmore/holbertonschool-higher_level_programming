8508ca65d7cf3438b91169944cfb98846d30690c
jaarmore <jaarmore@gmail.com>
2020-01-09 19:21:19 -0500

    Fix underscore on init

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Square Module"""
-class Square:
+
+
+class Square():
     """class Square that defines a Square"""
-    def __init_(self, size=0):
+    def __init__(self, size=0):
         """check if the size of square is an integer."""
         if isinstance(size, int):
             if size >= 0:
