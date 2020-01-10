615c7c9d5cd0fb577ea722545041a0ee66825cf9
jaarmore <jaarmore@gmail.com>
2020-01-09 19:16:39 -0500

    Fix docstring on file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+"""Square Module"""
 class Square:
     """class Square that defines a Square"""
-
     def __init_(self, size=0):
         """check if the size of square is an integer."""
         if isinstance(size, int):
-            if size < 0:
-                raise ValueError('size must be >= 0')
-            else:
+            if size >= 0:
                 self.__size = size
+            else:
+                raise ValueError('size must be >= 0')
         else:
             raise TypeError('size must be an integer')
