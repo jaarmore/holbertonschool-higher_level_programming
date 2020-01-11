6f22ebf9e6d7d1923e126e7ae3acf9520e90d508
jaarmore <jaarmore@gmail.com>
2020-01-11 03:47:03 +0000

    Fix comparison tuple == 2 elements

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -37,7 +37,7 @@ class Square():
     @position.setter
     def position(self, value):
         """Set the value of position"""
-        if (isinstance(value, tuple) and len(value) != 2 and
+        if (isinstance(value, tuple) and len(value) == 2 and
             isinstance(value[0], int) and isinstance(value[1], int) and
                 value[0] >= 0 and value[1] >= 0):
                 self.__position = value
