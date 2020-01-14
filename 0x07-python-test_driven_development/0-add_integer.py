66968b4d7a4e2972e6790870793e9d65adafec10
jaarmore <jaarmore@gmail.com>
2020-01-14 01:23:03 +0000

    Adding 0-add_integer.py, 0-add_integer.txt files to repository

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module contains add_integer function.
+Prototype def add_integer(a, b=98)
+Function that adds two integer numbers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Function that adds two integers
+
+    Args:
+        a (int): first integer
+        b (int): second integer
+
+    Returns:
+        int: the sum of a + b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+
+    if a == None:
+        raise TypeError('a must be an integer')
+
+    return int(a) + int(b)
