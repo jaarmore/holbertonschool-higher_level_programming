a5e0d0e6aae39cfb192b6c4e0134167663f6c4f2
jaarmore <jaarmore@gmail.com>
2020-01-14 15:47:22 -0500

    Adding 1-rectangle.py file to repository

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Class that defines a Rectangle with private properties width and height
+Prototype: class Rectangle()
+"""
+
+
+class Rectangle():
+    """
+    Class Rectangle that defines a Rectangle
+
+    Args:
+
+        width: width of rectangle
+        height: height of rectangle
+
+    Returns:
+        Nothing
+
+    """
+
+    def __init__(self, width=0, height=0):
+        """Instantiation of values width and height"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Returns the value of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Sets value of width only if value is an integer greather than zero
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        else:
+            if value < 0:
+                raise ValueError('width must be >= 0')
+            else:
+                self.__width = value
+
+    @property
+    def height(self):
+        """Returns the value of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Sets value of height only if value is an integer greather than zero
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        else:
+            if value < 0:
+                raise ValueError('height must be >= 0')
+            else:
+                self.__height = value
