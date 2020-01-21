6b7fb7b0ceb5436983dcb4a39335fd6bc588442f
jaarmore <jaarmore@gmail.com>
2020-01-21 00:33:05 -0500

    Adding 3-is_kind_of_class.py file to repository

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Same class or inherit from
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function that returns True if the object is an
+    instance of the specified class ; otherwise False.
+    """
+    return isinstance(obj, a_class)
