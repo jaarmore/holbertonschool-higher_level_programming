8bb06283f191e7494ec23fb376476c66d568cad6
jaarmore <jaarmore@gmail.com>
2020-01-21 00:55:46 -0500

    Adding 6-base_geometry.py file to repository

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Geometry module
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+    """
+    def area(self):
+        """
+        Empty method area() that raises an Exception
+        """
+        raise Exception('area() is not implemented')
