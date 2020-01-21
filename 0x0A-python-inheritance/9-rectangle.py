b80f1aa6061b726f4425dd23c56d560bed51016f
jaarmore <jaarmore@gmail.com>
2020-01-21 01:49:58 -0500

    Adding 9-rectangle.py file to repository

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Geometry module
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+    """
+    def area(self):
+        """
+        Empty method area() that raises an Exception
+        """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """
+        Public method that validates value
+        """
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+"""
+Class Rectangle that inherits from BaseGeometry
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class Rectangle
+    """
+    def __init__(self, width, height):
+        """
+        Initializes values of width and height
+
+        Args:
+           width: integer of width
+           height: integer of height
+        """
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+    """
+    Method that returns the area of Rectangle
+    """
+        return (self.__width * self.__height)
+
+    def __str__(self):
+    """
+    Prints the Rectangle description
+    """
+    return ("[Rectangle] {}/{}".format(self.__width, self.__height))
