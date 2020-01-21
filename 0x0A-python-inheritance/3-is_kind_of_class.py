234631f09cfc7f2269934f613d33a7c000ec5014
jaarmore <jaarmore@gmail.com>
2020-01-21 00:34:49 -0500

    fix def function on file

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,7 @@ Same class or inherit from
 """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """
     Function that returns True if the object is an
     instance of the specified class ; otherwise False.
