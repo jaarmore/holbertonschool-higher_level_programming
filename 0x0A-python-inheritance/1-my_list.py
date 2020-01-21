5a7bd1f55ed68974578bcffb93b397f9cb7f5c31
jaarmore <jaarmore@gmail.com>
2020-01-21 00:26:42 -0500

    fix stringdoc on file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,6 @@ Class MyList that inherits from list
 class MyList(list):
     """
     Function that prints a list in ascending sort.
-    """"
+    """
     def print_sorted(self):
         print(sorted(self))
