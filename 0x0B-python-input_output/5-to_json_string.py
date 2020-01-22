7a4f183199b1adfd816cceb2e1ee8ca50ce86dc6
jaarmore <jaarmore@gmail.com>
2020-01-21 22:43:39 -0500

    Adding 5-to_json_string.py file to repository

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""
+5. To JSON string module
+"""
+
+
+def to_json_string(my_obj):
+    """function that returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
