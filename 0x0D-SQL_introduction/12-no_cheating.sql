280d2b23ef299e6b5fc881add6cff75ca5a65e95
jaarmore <jaarmore@gmail.com>
2020-03-02 17:26:26 +0000

    Adding 12-no_cheating.sql file to repository

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,7 @@
+-- Script that updates the score of Bob to 10 in the table second_table.
+-- The database name will be passed as an argument.
+UPDATE second_table
+SET
+	score = 10
+WHERE
+	name = 'Bob';
