5b193d8fd953032316ce09d5d2822a3d7e944ef5
jaarmore <jaarmore@gmail.com>
2020-03-02 17:41:18 +0000

    Adding 14-average.sql file to repository

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- Script that computes the score average of all records in the table second_table.
+-- The result column name should be average.
+SELECT
+	AVG(score) 'average'
+FROM
+	second_table;
