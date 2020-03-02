e289fe2d68d51822d8de86f739f7d078cf86c825
jaarmore <jaarmore@gmail.com>
2020-03-02 15:47:29 +0000

    Fix issue on file with comma symbol

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,5 +2,5 @@
 -- If the table first_table already exists, your script should not fail.
 CREATE TABLE IF NOT EXISTS first_table (
        id INT,
-       name VARCHAR(256);
+       name VARCHAR(256)
 );
