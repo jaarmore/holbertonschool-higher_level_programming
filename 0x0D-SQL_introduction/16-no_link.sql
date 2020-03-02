3a98a207a2c5e32a37a23454f2979d5b168a1fb1
jaarmore <jaarmore@gmail.com>
2020-03-02 19:16:36 +0000

    Adding 16-no_link.sql file to repository

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,10 @@
+-- Script that lists all records of the table second_table.
+-- Records should be listed by descending score.
+SELECT
+	score, name
+FROM
+	second_table
+WHERE
+	name != ''
+ORDER BY
+      score DESC;
