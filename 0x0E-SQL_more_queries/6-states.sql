3b4d0792dd5cfa53434429737aa450fec7df2517
jaarmore <jaarmore@gmail.com>
2020-03-04 04:45:43 +0000

    fix EXISTS on create table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,7 @@
 -- If the database hbtn_0d_usa or table states already exists, your script should not fail.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXIST states (
+CREATE TABLE IF NOT EXISTS states (
        id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
        name VARCHAR(256)
 );
