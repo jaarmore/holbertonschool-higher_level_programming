8c937a24306ffb3e4b03b576dd51ff0718f01c3d
jaarmore <jaarmore@gmail.com>
2020-03-04 04:43:28 +0000

    Adding USE command to file

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- script that creates the database hbtn_0d_usa and the table states.
 -- If the database hbtn_0d_usa or table states already exists, your script should not fail.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXIST states (
        id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
        name VARCHAR(256)
