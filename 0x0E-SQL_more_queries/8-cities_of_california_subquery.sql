f206d2cf26be84028a8f8745c07a38cb26152645
jaarmore <jaarmore@gmail.com>
2020-03-04 05:28:07 +0000

    Adding 8-cities_of_california_subquery.sql file to repository

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+-- Results must be sorted in ascending order by cities.id.
+SELECT id, name
+FROM cities
+WHERE id =
+      (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
