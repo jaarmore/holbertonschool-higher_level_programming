23a192c7df7be7612af393471f4f2b1e4f62e831
jaarmore <jaarmore@gmail.com>
2020-03-04 05:31:32 +0000

    Fix id by state_id on query

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,6 @@
 -- Results must be sorted in ascending order by cities.id.
 SELECT id, name
 FROM cities
-WHERE id =
+WHERE state_id =
       (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
