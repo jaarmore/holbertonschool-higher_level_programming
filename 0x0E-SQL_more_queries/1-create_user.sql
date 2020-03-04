4e935d5a722b9af87e6d759f025ca3c2e863425d
jaarmore <jaarmore@gmail.com>
2020-03-04 04:04:51 +0000

    Adding 1-create_user.sql file to repository

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- script that creates the user user_0d_1.
+-- If the user user_0d_1 already exists, your script should not fail.
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
