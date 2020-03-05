c8c46bce9c9e49a165a1cd522ac586f7010bb688
jaarmore <jaarmore@gmail.com>
2020-03-04 19:50:18 -0500

    Adding 101-avg_temperatures.sql file to repository

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+-- Import in hbtn_0c_0 database.
+SELECT city, AVG(value) 'avg_temp'
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
