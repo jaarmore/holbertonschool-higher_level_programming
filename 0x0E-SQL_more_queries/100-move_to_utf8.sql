b789ea249ca48d4a1127f18761738aef7b5afea5
jaarmore <jaarmore@gmail.com>
2020-03-04 20:17:51 -0500

    Remove file 100-move_to_utf8.sql from repository

--- 0x0E-SQL_more_queries/100-move_to_utf8.sql
+++
@@ -1,5 +0,0 @@
--- Script that converts hbtn_0c_0 database to UTF8.
--- You need convert to UTF8 Database hbtn_0c_0, Table first_table, Field name in first_table.
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
