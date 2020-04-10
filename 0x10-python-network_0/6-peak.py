57798ced19b1c86225fcb951b866272abad628f0
jaarmore <jaarmore@gmail.com>
2020-04-10 05:14:48 +0000

    fix pep8 on file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Function that finds a peak in a list of unsorted integers. """
 
+
 def find_peak(list_of_integers):
     if list_of_integers:
         return (max(list_of_integers))
