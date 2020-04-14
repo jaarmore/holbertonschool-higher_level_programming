290ad6fc5df1afaf412154112bc1ef9d1403429a
jaarmore <jaarmore@gmail.com>
2020-04-14 23:43:51 +0000

    updating code on file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,4 +4,14 @@
 
 def find_peak(list_of_integers):
     if list_of_integers:
-        return (max(list_of_integers))
+        aux = list_of_integers
+        start = 0
+        end = len(list_of_integers) - 1
+        mid = (start + end) // 2
+
+        if (aux[mid - 1] <= aux[mid] and aux[mid + 1] <= aux[mid]):
+            return aux[mid]
+        elif (aux[mid] < aux[mid + 1]):
+            return find_peak(aux[mid + 1:])
+        else:
+            return find_peak(aux[:mid - 1])
