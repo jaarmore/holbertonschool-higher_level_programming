807a06f029b857caea61243a6fd84841109328dc
jaarmore <jaarmore@gmail.com>
2020-04-15 04:11:33 +0000

    Adding task 0 file to repository

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+        data = resp.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(data)))
+        print('\t- content: {}'.format(data))
+        print('\t- utf8 content: {}'.format(data.decode("utf-8", "replace")))
