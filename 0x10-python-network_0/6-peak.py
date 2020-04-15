bf104da99e53e1496f9272be5e4e6abf7a8e8347
Jackson Moreno <43226501+jaarmore@users.noreply.github.com>
2020-04-14 21:19:17 -0500

    Fix code and update efficience

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,15 +3,23 @@
 
 
 def find_peak(list_of_integers):
-    if list_of_integers:
-        aux = list_of_integers
-        start = 0
-        end = len(list_of_integers) - 1
-        mid = (start + end) // 2
+    """
+    Find a peak in a list of unsorted integers.
+    """
+    if len(list_of_integers) == 0:
+        return None
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
 
-        if (aux[mid - 1] <= aux[mid] and aux[mid + 1] <= aux[mid]):
-            return aux[mid]
-        elif (aux[mid] < aux[mid + 1]):
-            return find_peak(aux[mid + 1:])
-        else:
-            return find_peak(aux[:mid - 1])
+    high = len(list_of_integers) - 1
+    low = 0
+    lis = list_of_integers
+    while high > low:
+        mid = (high + low) // 2
+        if lis[mid] <= lis[mid + 1]:
+            low = mid + 1
+        elif lis[mid] <= lis[mid - 1]:
+            high = mid - 1
+        elif lis[mid] >= lis[mid + 1] and lis[mid] >= lis[mid - 1]:
+            return lis[mid]
+    return lis[low]
