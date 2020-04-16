018a27e3aac16755a8ea6e9de0d0573a4fa1a3b1
jaarmore <jaarmore@gmail.com>
2020-04-16 03:04:47 +0000

    Adding first advance task file to repository

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script that takes 2 arguments owner and repo and
+list 10 commits (from the most recent to oldest).
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    owner = sys.argv[1]
+    repo = sys.argv[2]
+    URL = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    cont = 0
+
+    r = requests.get(URL)
+    for commit in r.json():
+        user = commit.get('commit').get('author').get('name')
+        user_sha = commit.get('sha')
+        print("{}: {}".format(user_sha, user))
+        cont += 1
+        if cont > 9:
+            break
