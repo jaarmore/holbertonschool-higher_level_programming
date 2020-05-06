c73fb1909c0d70a73e356ec771a90689edd65178
jaarmore <jaarmore@gmail.com>
2020-05-06 02:12:40 +0000

    fix code on file

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,8 @@ const args = process.argv;
 if (args.length === 2 || args.length === 3) {
   console.log(0);
 } else {
-  const params = args.slice(2);
-  params.sort();
+  const params = args.slice(2).map(Number);
+  params.sort(function (a, b) { return a - b; });
   const second = params.length - 2;
   console.log(params[second]);
 }
