be4150399cd1077106225bacd794015b3d2e65cf
jaarmore <jaarmore@gmail.com>
2020-05-06 00:57:05 +0000

    Fix code on file, removing console.log(params), and changing sort function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,8 +4,7 @@ if (args.length === 2 || args.length === 3) {
   console.log(0);
 } else {
   const params = args.slice(2);
-  console.log(params);
-  console.log(params.sort());
+  params.sort();
   const second = params.length - 2;
   console.log(params[second]);
 }
