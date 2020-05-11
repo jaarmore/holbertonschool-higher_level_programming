a2ff88688bdbed5f79a3507e039aabdad0e87c84
jaarmore <jaarmore@gmail.com>
2020-05-11 23:12:21 +0000

    Remove reverse() function and use loop to reverse array

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  const myArray = [];
+  for (let i = 0; i < list.length; i++) {
+    myArray.unshift(list[i]);
+  }
+  return myArray;
 };
