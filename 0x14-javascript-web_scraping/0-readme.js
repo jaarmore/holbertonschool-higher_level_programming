9e44b8c81af28a0bfccdde05cb2d5f072528b8b2
jaarmore <jaarmore@gmail.com>
2020-05-12 19:21:47 +0000

    Adding else to fix the output

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 fs.readFile(args[2], 'utf-8', (error, data) => {
   if (error) {
     console.error(error);
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
