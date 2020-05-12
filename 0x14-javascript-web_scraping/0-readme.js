76e928fbfb5f1f1743d4147b57c538918fc32423
jaarmore <jaarmore@gmail.com>
2020-05-12 20:34:45 +0000

    Adding task 0 file to repository

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-// Script that reads and prints the content of a file.
+/*
+ * Script that reads and prints the content of a file.
+ */
 const args = process.argv;
 const fs = require('fs');
 
 fs.readFile(args[2], 'utf-8', function (error, data) {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else {
     console.log(data);
   }
