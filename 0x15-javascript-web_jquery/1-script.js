4a3289f60ca7654cadfc9d438711b3a7ee93c01e
jaarmore <jaarmore@gmail.com>
2020-05-14 23:09:29 +0000

    Adding task 1 file to repository

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+ * Script that updates the text color of the HTML tag HEADER to red (#FF0000)
+ * using the JQuery API.
+ */
+$(document).ready(function () {
+  $('header').css('color', '#FF0000');
+});
