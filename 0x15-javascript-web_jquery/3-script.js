c5bd1d995ea0a7ddcc6ef1fac335986e0d810caa
jaarmore <jaarmore@gmail.com>
2020-05-15 01:25:08 +0000

    Adding task 3 file to repository

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+ * Script that adds the class red to the HTML tag HEADER to red (#FF0000)
+ * when the user clicks on the tag DIV#red_header.
+ */
+$(document).ready(function () {
+  $('#red_header').click(function () {
+    $('header').addClass('red');
+  });
+});
