7ffb7ed33a455379f16e6f73f1ea5bd34e3a052e
jaarmore <jaarmore@gmail.com>
2020-05-15 02:32:03 +0000

    Adding task 6 file to repository

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+ * Script updates the text of the HTML tag HEADER to New Header!!!
+ * when the user clicks on DIV#update_header
+ */
+$(document).ready(function () {
+  $('#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
+});
